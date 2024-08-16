15219a34d9b653d16d2ef1de3a6db0e58c3d10dd
Gustavo Henrique <gustavo329886@gmail.com>
2024-08-16 18:46:09 -0300

    Configuração do repositório back-end

---
+++ src/main/java/school/sptech/vannbora/VannboraApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.vannbora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VannboraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VannboraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/school/sptech/vannbora/VannboraApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.vannbora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VannboraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
