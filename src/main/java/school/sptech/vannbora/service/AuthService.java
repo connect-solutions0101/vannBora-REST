ccc39e2614791d45e9fd8860818bbfc50695f15b
Kauan Figueiredo Lucena <kauanfigueiredolucena2016@gmail.com>
2024-10-03 12:59:33 -0300

    feat: Adiciona autenticação e autorização com Spring Security e JWT

---
+++ src/main/java/school/sptech/vannbora/service/AuthService.java
@@ -0,0 +1,19 @@
+package school.sptech.vannbora.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import school.sptech.vannbora.repository.ProprietarioServicoRepository;
+
+@Service
+public class AuthService implements UserDetailsService {
+    @Autowired
+    ProprietarioServicoRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username);
+    }
+}
