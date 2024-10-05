893c1741b89966e7739f4463fa08aa496375e7fa
Kauan Figueiredo Lucena <kauanfigueiredolucena2016@gmail.com>
2024-10-05 01:28:59 -0300

    feat(Endereco mapper): Adiciona mapper para entidade Endereco

--- src/main/java/school/sptech/vannbora/controller/EnderecoController.java
+++ src/main/java/school/sptech/vannbora/controller/EnderecoController.java
@@ -3,9 +3,13 @@ package school.sptech.vannbora.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import school.sptech.vannbora.dto.endereco.EnderecoRequestDto;
+import school.sptech.vannbora.dto.endereco.EnderecoResponseDto;
 import school.sptech.vannbora.entidade.Endereco;
+import school.sptech.vannbora.mapper.EnderecoMapper;
 import school.sptech.vannbora.service.EnderecoService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,33 +19,43 @@ public class EnderecoController {
     @Autowired
     private EnderecoService service;
 
+    @Autowired
+    private EnderecoMapper mapper;
+
     @GetMapping
-    public ResponseEntity<List<Endereco>> listar(){
+    public ResponseEntity<List<EnderecoResponseDto>> listar(){
         List<Endereco> lista = service.listar();
 
         if(lista.isEmpty()) return ResponseEntity.status(204).build();
 
-        return ResponseEntity.status(200).body(lista);
+        List<EnderecoResponseDto> dtoLista = new ArrayList<>();
+        for(Endereco endereco : lista){
+            dtoLista.add(mapper.toEnderecoResponseDto(endereco));
+        }
+
+        return ResponseEntity.status(200).body(dtoLista);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Endereco> buscarPorId(@PathVariable int id){
-        return ResponseEntity.status(200).body(service.buscarPorId(id));
+    public ResponseEntity<EnderecoResponseDto> buscarPorId(@PathVariable int id){
+        return ResponseEntity.status(200).body(mapper.toEnderecoResponseDto(service.buscarPorId(id)));
     }
 
     @GetMapping("/cep")
-    public ResponseEntity<Endereco> buscarPorCep(@RequestParam String cep){
-        return ResponseEntity.status(200).body(service.buscarPorCep(cep));
+    public ResponseEntity<EnderecoResponseDto> buscarPorCep(@RequestParam String cep){
+        return ResponseEntity.status(200).body(mapper.toEnderecoResponseDto(service.buscarPorCep(cep)));
     }
 
     @PostMapping
-    public ResponseEntity<Endereco> cadastrar(@RequestBody Endereco endereco){
-        return ResponseEntity.status(201).body(service.cadastrar(endereco));
+    public ResponseEntity<EnderecoResponseDto> cadastrar(@RequestBody EnderecoRequestDto endereco){
+        Endereco novoEndereco = mapper.toEndereco(endereco);
+        return ResponseEntity.status(201).body(mapper.toEnderecoResponseDto(service.cadastrar(novoEndereco)));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Endereco> atualizar(@PathVariable int id, @RequestBody Endereco endereco){
-        return ResponseEntity.status(200).body(service.atualizar(id, endereco));
+    public ResponseEntity<EnderecoResponseDto> atualizar(@PathVariable int id, @RequestBody EnderecoRequestDto endereco){
+        Endereco enderecoEditado = mapper.toEnderecoAtualizar(id, endereco);
+        return ResponseEntity.status(200).body(mapper.toEnderecoResponseDto(service.atualizar(id, enderecoEditado)));
     }
 
     @DeleteMapping("/{id}")
