de7f70fa37ec892e0651110e9223a78283751cb6
KauanLuc <kauan.lucena@sptech.school>
2024-12-10 10:24:57 -0300

    fix: Utiliza id no endpoint embarque

--- src/main/java/school/sptech/vannbora/controller/TrajetoController.java
+++ src/main/java/school/sptech/vannbora/controller/TrajetoController.java
@@ -3,18 +3,18 @@ package school.sptech.vannbora.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import school.sptech.vannbora.dto.dependente.DependenteRequestDto;
-import school.sptech.vannbora.dto.dependente.DependenteResponseDto;
 import school.sptech.vannbora.dto.dependente.DependenteResponseResumoDto;
 import school.sptech.vannbora.entidade.Dependente;
 import school.sptech.vannbora.entidade.FilaObj;
 import school.sptech.vannbora.entidade.PilhaObj;
-import school.sptech.vannbora.mapper.DependenteMapper;
+import school.sptech.vannbora.service.DependenteService;
 
 @RestController
 @RequestMapping("/trajeto")
 @RequiredArgsConstructor
 public class TrajetoController {
+
+    private final DependenteService dependenteService;
 
     PilhaObj<Dependente> pilha = new PilhaObj<>(30);
     private final FilaObj<Dependente> fila = new FilaObj<>(new Dependente[30], 0, -1, 0);
@@ -35,10 +35,10 @@ public class TrajetoController {
     }
 
     @PostMapping("/embarque")
-    public ResponseEntity embarque(@RequestBody DependenteRequestDto dto){
+    public ResponseEntity embarque(@PathVariable int id){
         if(pilha.isFull()) return ResponseEntity.status(429).body("A pilha está cheia. Desembarque um dependente para embarcar outro.");
 
-        Dependente dependente = DependenteMapper.toDependente(dto);
+        Dependente dependente = dependenteService.buscarPorId(id);
         pilha.push(dependente);
         fila.insert(dependente);
         return ResponseEntity.ok().build();
@@ -51,11 +51,6 @@ public class TrajetoController {
         }
 
         Dependente dependenteDesembarcado = pilha.pop();
-        Dependente dependenteRemovidoDaFila = fila.poll();
-
-        if (!dependenteDesembarcado.equals(dependenteRemovidoDaFila)) {
-            return ResponseEntity.status(500).body("Erro de sincronização entre pilha e fila.");
-        }
 
         return ResponseEntity.ok().build();
     }
