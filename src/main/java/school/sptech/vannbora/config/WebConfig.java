7d8ed8ae37ec19fa2ea3c973fce6170f5a8009d5
Gustavo Henrique <gustavo329886@gmail.com>
2025-05-29 16:26:22 -0300

    fix/correção no CORS

--- src/main/java/school/sptech/vannbora/config/WebConfig.java
+++ src/main/java/school/sptech/vannbora/config/WebConfig.java
@@ -16,6 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000") // Adicione o domínio do seu frontend
                         .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://52.205.245.186:80")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
